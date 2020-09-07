e63af6627d101aee4b08fd6a68268bd332a6d87b
pegasis <admin@pegasis.site>
2020-09-07 18:09:40 -0400

    websocket, online users

---
+++ frontend/src/ServerCon.js
@@ -0,0 +1,43 @@
+import uuid from "uuid-random"
+
+export default class ServerCon {
+    queue = []
+    pendingReqs = new Map()
+    onmessage = (_) => {
+    }
+
+    constructor(addr) {
+        this.ws = new WebSocket(addr)
+
+        this.ws.onopen = () => {
+            this.queue.forEach((json) => {
+                this.ws.send(JSON.stringify(json))
+            })
+            this.queue = []
+        }
+        this.ws.onmessage = (e) => {
+            let json = JSON.parse(e.data)
+            let id = json["id"]
+            let resolutionFunc = this.pendingReqs.get(id)
+            if (resolutionFunc) {
+                resolutionFunc(json)
+            } else {
+                this.onmessage(json)
+            }
+        }
+    }
+
+    request(json) {
+        return new Promise((resolutionFunc, _) => {
+            const id = uuid()
+            this.pendingReqs.set(id, resolutionFunc)
+
+            json["id"] = id
+            if (this.ws.readyState !== 1) {
+                this.queue.push(json)
+            } else {
+                this.ws.send(JSON.stringify(json))
+            }
+        })
+    }
+}
--- frontend/src/chat.js
+++ frontend/src/chat.js
@@ -1,10 +1,43 @@
 import {Component} from "preact"
 import {getName} from "./tools"
+import ServerCon from "./ServerCon"
 
 export default class ChatPage extends Component {
-    render(){
+    state = {
+        users: [],
+        messages: [],
+    }
+
+    constructor(props) {
+        super(props)
+        this.connection = new ServerCon("ws://localhost:8080")
+        this.connection.onmessage = (json) => {
+            if (json["typ"] === "users") {
+                this.setState({
+                    users: json["users"].sort(),
+                })
+            }
+        }
+
+        this.connection.request({
+            "typ": "name",
+            "name": getName(),
+        }).then((json) => {
+            this.setState({
+                users: json["users"].sort(),
+                messages: json["messages"],
+            })
+        })
+    }
+
+    render() {
         return <div>
-            {getName()}
+            <div>
+                <h2>Online Users</h2>
+                <ul>
+                    {this.state.users.map((name) => <li key={name}>{name}</li>)}
+                </ul>
+            </div>
         </div>
     }
 }
