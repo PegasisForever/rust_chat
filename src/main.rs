8c5d99317a709c827779916ff7ae411c404b0183
pegasis <admin@pegasis.site>
2020-09-07 12:12:42 -0400

    user join & leave

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,20 @@ use std::{env, io::Error};
 use log::info;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::mpsc;
-use tokio::sync::broadcast;
-use tokio::sync::broadcast::{Sender, Receiver};
+use tokio::sync::mpsc::{Sender, Receiver};
 use tokio_tungstenite::tungstenite::Message;
 use futures_util::{StreamExt, SinkExt};
 use futures_util::core_reexport::result::Result::Ok;
 use serde_json::{Value};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::iter::FromIterator;
 use crate::structs::{UserChange, ChangeType, OnlineUsersBoardCast, NameReq, MsgReq, MsgBoardCast, CurrentStateRes};
 use tokio::signal;
 use std::sync::{Mutex, Arc};
 use serde_json::json;
-use futures_util::core_reexport::sync::atomic::Ordering::AcqRel;
 
-type Messages = Arc<Mutex<Vec<MsgBoardCast>>>;
+
+type UsersMap = Arc<Mutex<HashMap<String, Sender<String>>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -29,19 +28,12 @@ async fn main() -> Result<(), Error> {
     let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
     info!("Listening");
 
-    let (ws_tx, _) = broadcast::channel(32);
-    let (user_tx, user_rx) = mpsc::channel(32);
-
-    let messages: Messages = Arc::new(Mutex::new(Vec::new()));
-    let messages_global = messages.clone();
+    let users_map: UsersMap = Arc::new(Mutex::new(HashMap::new()));
 
     tokio::select! {
-        _ = user_manager(ws_tx.clone(), user_rx) => (),
         _ = async move {
             while let Ok((stream, _)) = listener.accept().await {
-                let ws_tx = ws_tx.clone();
-                let ws_rx = ws_tx.subscribe();
-                tokio::spawn(process_connection(stream, ws_tx, ws_rx, user_tx.clone(), messages.clone()));
+                tokio::spawn(process_connection(stream, users_map.clone()));
             }
         } => (),
          _ = signal::ctrl_c() => (),
@@ -51,30 +43,38 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn user_manager(ws_tx: Sender<String>, mut user_rx: mpsc::Receiver<UserChange>) {
-    let mut users = HashSet::new();
-    while let Some(change) = user_rx.recv().await {
-        match change.change_type {
-            ChangeType::Join => users.insert(change.name),
-            ChangeType::Leave => users.remove(&change.name),
-        };
+fn user_joined(name: &Option<String>, users_map: &UsersMap, ws_tx: &Sender<String>) {
+    users_map.lock().unwrap().insert(String::from(name.as_ref().unwrap()), ws_tx.clone());
+    send_user_list_bc(users_map);
+}
 
-        let bc = OnlineUsersBoardCast {
-            typ: "users".to_string(),
-            users: Vec::from_iter(users.clone().into_iter()),
-        };
-        ws_tx.send(serde_json::to_string(&bc).unwrap()).unwrap();
+fn user_left(name: &Option<String>, users_map: &UsersMap) {
+    users_map.lock().unwrap().remove(name.as_ref().unwrap());
+    send_user_list_bc(users_map);
+}
+
+fn send_user_list_bc(users_map: &UsersMap) {
+    let mut users_map = users_map.lock().unwrap();
+    let user_list = users_map.iter()
+        .map(|(name, _)| {
+            String::from(name)
+        })
+        .collect::<Vec<String>>();
+    let bc = OnlineUsersBoardCast {
+        typ: "users".to_string(),
+        users: user_list,
+    };
+    let text = serde_json::to_string(&bc).unwrap();
+    for (_, tx) in users_map.iter_mut() {
+        tx.try_send(text.clone()).unwrap();
     }
 }
 
-async fn process_connection(stream: TcpStream,
-                            ws_tx: Sender<String>,
-                            mut ws_rx: Receiver<String>,
-                            mut user_tx: mpsc::Sender<UserChange>,
-                            messages: Messages) {
+async fn process_connection(stream: TcpStream, users_map: UsersMap) {
     info!("New WebSocket connection");
     let (mut write, mut read) =
         tokio_tungstenite::accept_async(stream).await.unwrap().split();
+    let (mut ws_tx, mut ws_rx) = mpsc::channel::<String>(32);
 
     let receive_task = async move {
         let mut name: Option<String> = None;
@@ -87,15 +87,13 @@ async fn process_connection(stream: TcpStream,
                                 name = Some(req.name);
                                 info!("name set to {:?}", name.as_ref().unwrap());
 
-                                let user_change = UserChange {
-                                    change_type: ChangeType::Join,
-                                    name: String::from(name.as_ref().unwrap()),
-                                };
-                                user_tx.send(user_change).await.unwrap();
+                                user_joined(&name, &users_map, &ws_tx);
 
-                                let message_list = messages.lock().unwrap();
-                                let res = json!(&*message_list);
-                                write.lock().unwrap().send(Message::Text(res.to_string())).await.unwrap();
+                                // let user_change = UserChange {
+                                //     change_type: ChangeType::Join,
+                                //     name: String::from(name.as_ref().unwrap()),
+                                // };
+                                // user_tx.send(user_change).await.unwrap();
                             }
                         } else if json["typ"] == "msg" {
                             if let Ok(req) = serde_json::from_value::<MsgReq>(json.clone()) {
@@ -105,8 +103,8 @@ async fn process_connection(stream: TcpStream,
                                     name: String::from(name.as_ref().unwrap()),
                                     text: req.text,
                                 };
-                                ws_tx.send(serde_json::to_string(&bc).unwrap()).unwrap();
-                                messages.lock().unwrap().push(bc);
+                                // ws_tx.send(serde_json::to_string(&bc).unwrap()).unwrap();
+                                // messages.lock().unwrap().push(bc);
                             }
                         } else if json["type"] == "chess" {
                             unimplemented!();
@@ -118,15 +116,16 @@ async fn process_connection(stream: TcpStream,
             }
         }
 
-        let user_change = UserChange {
-            change_type: ChangeType::Leave,
-            name: String::from(name.as_ref().unwrap()),
-        };
-        user_tx.send(user_change).await.unwrap();
+        user_left(&name, &users_map);
+        // let user_change = UserChange {
+        //     change_type: ChangeType::Leave,
+        //     name: String::from(name.as_ref().unwrap()),
+        // };
+        // user_tx.send(user_change).await.unwrap();
     };
 
     let send_task = async move {
-        while let Ok(msg) = ws_rx.recv().await {
+        while let Some(msg) = ws_rx.recv().await {
             write.send(Message::Text(msg)).await.unwrap();
         }
     };
