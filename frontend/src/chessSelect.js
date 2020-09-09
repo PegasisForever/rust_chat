4a4fe07d22e7ee5978d598758b18d45648c94a24
pegasis <admin@pegasis.site>
2020-09-08 20:33:49 -0400

    refactor image pos

--- frontend/src/chessSelect.js
+++ frontend/src/chessSelect.js
@@ -7,11 +7,11 @@ function SelectItem(props) {
 
 export default function ChessSelect(props) {
     return <div class="chess-select-bar">
-        <SelectItem src={"select_black.svg"} text={"Black"} isSelected={props.isBlack === true}
+        <SelectItem src="assets/select_black.svg" text={"Black"} isSelected={props.isBlack === true}
                     onClick={()=>props.onChange(true)}/>
-        <SelectItem src={"select_white.svg"} text={"White"} isSelected={props.isBlack === false}
+        <SelectItem src="assets/select_white.svg" text={"White"} isSelected={props.isBlack === false}
                     onClick={()=>props.onChange(false)}/>
-        <SelectItem src={"select_none.svg"} text={"Remove"} isSelected={props.isBlack === null}
+        <SelectItem src="assets/select_none.svg" text={"Remove"} isSelected={props.isBlack === null}
                     onClick={()=>props.onChange(null)}/>
     </div>
 }
